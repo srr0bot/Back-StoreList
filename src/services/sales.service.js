1889d2bc72ac037bd987d944661bc61a1b3f0b4c
DonovanPS <91863877+srr0bot@users.noreply.github.com>
2024-03-07 10:55:57 -0500

    totales de productos y monto de ventas por IVA

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -118,11 +118,13 @@ module.exports = {
   
 
       const groupedByIva = await this.findSalesByIVA(month, year);
+      const detailSalesByIva = await this.findDetailedSalesByIVA(month, year);
 
       return {
         sales: ventas,
         statistics: salesByMonth,
-        groupedByIva: groupedByIva
+        groupedByIva: groupedByIva, 
+        salesByIva: detailSalesByIva
       };
     } catch (error) {
       throw error;
@@ -155,83 +157,182 @@ module.exports = {
 
   async findSalesByIVA(month, year) {
     try {
-      // Validar que el mes esté en el rango de 1 a 12
-      if (month < 1 || month > 12) {
-        throw new Error("El número de mes debe estar entre 1 y 12");
-      }
-  
-      // Obtener el primer día del mes
-      const startDate = new Date(year, month - 1, 1);
-      
-      // Obtener el último día del mes
-      const endDate = new Date(year, month, 0);
-      endDate.setUTCHours(23, 59, 59, 999);
-  
-      // Realizar la agregación para obtener las ventas agrupadas por IVA
-      const salesByIVA = await SalesModule.aggregate([
-        {
-          $match: {
-            date: { $gte: startDate, $lte: endDate }
-          }
-        },
-        {
-          $unwind: "$products"
-        },
-        {
-          $lookup: {
-            from: "products",
-            localField: "products.product",
-            foreignField: "_id",
-            as: "productInfo"
-          }
-        },
-        {
-          $unwind: "$productInfo"
-        },
-        {
-          $group: {
-            _id: { iva: "$productInfo.IVA", date: { $dateToString: { format: "%m/%d/%Y", date: "$date" } } },
-            totalAmount: { $sum: "$products.quantity" }
-          }
-        },
-        {
-          $sort: {
-            "_id.date": 1
-          }
+        // Validar que el mes esté en el rango de 1 a 12
+        if (month < 1 || month > 12) {
+            throw new Error("El número de mes debe estar entre 1 y 12");
         }
-      ]);
-  
-      // Estructurar los datos en el formato deseado
-      const labels = [...new Set(salesByIVA.map(item => item._id.date))];
-      const series = [
-        {
-          name: 'Iva 0',
-          data: labels.map(label => {
-            const sale = salesByIVA.find(item => item._id.date === label && item._id.iva === 0);
-            return sale ? sale.totalAmount : 0;
-          })
-        },
-        {
-          name: 'Iva 5',
-          data: labels.map(label => {
-            const sale = salesByIVA.find(item => item._id.date === label && item._id.iva === 5);
-            return sale ? sale.totalAmount : 0;
-          })
-        },
-        {
-          name: 'Iva 19',
-          data: labels.map(label => {
-            const sale = salesByIVA.find(item => item._id.date === label && item._id.iva === 19);
-            return sale ? sale.totalAmount : 0;
-          })
-        }
-      ];
-  
-      return { labels, series };
+
+        // Obtener el primer día del mes
+        const startDate = new Date(year, month - 1, 1);
+
+        // Obtener el último día del mes
+        const endDate = new Date(year, month, 0);
+        endDate.setUTCHours(23, 59, 59, 999);
+
+        // Realizar la agregación para obtener las ventas agrupadas por IVA
+        const salesByIVA = await SalesModule.aggregate([
+            {
+                $match: {
+                    date: { $gte: startDate, $lte: endDate }
+                }
+            },
+            {
+                $unwind: "$products"
+            },
+            {
+                $lookup: {
+                    from: "products",
+                    localField: "products.product",
+                    foreignField: "_id",
+                    as: "productInfo"
+                }
+            },
+            {
+                $unwind: "$productInfo"
+            },
+            {
+                $group: {
+                    _id: { iva: "$productInfo.IVA", date: { $dateToString: { format: "%m/%d/%Y", date: "$date" } } },
+                    totalAmount: { $sum: "$products.quantity" },
+                    totalPrice: { $sum: { $multiply: ["$products.quantity", "$productInfo.price"] } },
+                    totalProducts: { $sum: "$products.quantity" } // Contar la cantidad total de productos
+                }
+            },
+            {
+                $sort: {
+                    "_id.date": 1
+                }
+            }
+        ]);
+
+        // Estructurar los datos en el formato deseado
+        const labels = [...new Set(salesByIVA.map(item => item._id.date))];
+        const series = [
+            {
+                name: 'Iva 0',
+                data: labels.map(label => {
+                    const sale = salesByIVA.find(item => item._id.date === label && item._id.iva === 0);
+                    return sale ? sale.totalAmount : 0;
+                }),
+                totalProducts: salesByIVA.filter(item => item._id.iva === 0).reduce((total, item) => total + item.totalProducts, 0),
+                totalPrice: salesByIVA.filter(item => item._id.iva === 0).reduce((total, item) => total + item.totalPrice, 0)
+            },
+            {
+                name: 'Iva 5',
+                data: labels.map(label => {
+                    const sale = salesByIVA.find(item => item._id.date === label && item._id.iva === 5);
+                    return sale ? sale.totalAmount : 0;
+                }),
+                totalProducts: salesByIVA.filter(item => item._id.iva === 5).reduce((total, item) => total + item.totalProducts, 0),
+                totalPrice: salesByIVA.filter(item => item._id.iva === 5).reduce((total, item) => total + item.totalPrice, 0)
+            },
+            {
+                name: 'Iva 19',
+                data: labels.map(label => {
+                    const sale = salesByIVA.find(item => item._id.date === label && item._id.iva === 19);
+                    return sale ? sale.totalAmount : 0;
+                }),
+                totalProducts: salesByIVA.filter(item => item._id.iva === 19).reduce((total, item) => total + item.totalProducts, 0),
+                totalPrice: salesByIVA.filter(item => item._id.iva === 19).reduce((total, item) => total + item.totalPrice, 0)
+            }
+        ];
+
+        return { labels, series };
     } catch (error) {
-      throw error;
+        throw error;
     }
-  }
+},
+
+
+
+  async findDetailedSalesByIVA(month, year) {
+      try {
+        // Validar que el mes esté en el rango de 1 a 12
+        if (month < 1 || month > 12) {
+          throw new Error("El número de mes debe estar entre 1 y 12");
+        }
+    
+        // Obtener el primer día del mes
+        const startDate = new Date(year, month - 1, 1);
+        
+        // Obtener el último día del mes
+        const endDate = new Date(year, month, 0);
+        endDate.setUTCHours(23, 59, 59, 999);
+    
+        // Realizar la agregación para obtener las ventas agrupadas por IVA
+        const salesByIVA = await SalesModule.aggregate([
+          {
+            $match: {
+              date: { $gte: startDate, $lte: endDate }
+            }
+          },
+          {
+            $unwind: "$products"
+          },
+          {
+            $lookup: {
+              from: "products",
+              localField: "products.product",
+              foreignField: "_id",
+              as: "productInfo"
+            }
+          },
+          {
+            $unwind: "$productInfo"
+          },
+          {
+            $group: {
+              _id: "$productInfo.IVA",
+              sales: {
+                $push: {
+                  saleId: "$_id",
+                  date: { $dateToString: { format: "%m/%d/%Y", date: "$date" } },
+                  totalAmount: { $sum: "$products.quantity" },
+                  products: {
+                    productId: "$productInfo._id",
+                    productName: "$productInfo.name",
+                    productPrice: "$productInfo.price",
+                    productQuantity: "$products.quantity",
+                    productDate: "$date"
+                  }
+                }
+              }
+            }
+          }
+        ]);
+    
+        // Estructurar los datos en el formato deseado
+        const salesByIVADetails = {
+          "IVA 0%": [],
+          "IVA 5%": [],
+          "IVA 19%": []
+        };
+    
+        salesByIVA.forEach(sale => {
+          let ivaType;
+          switch (sale._id) {
+            case 0:
+              ivaType = "IVA 0%";
+              break;
+            case 5:
+              ivaType = "IVA 5%";
+              break;
+            case 19:
+              ivaType = "IVA 19%";
+              break;
+            default:
+              ivaType = "Sin IVA";
+          }
+          salesByIVADetails[ivaType] = sale.sales;
+        });
+    
+        return salesByIVADetails;
+      } catch (error) {
+        throw error;
+      }
+    }
+    
+  
   
   
   
